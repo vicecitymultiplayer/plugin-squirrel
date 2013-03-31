c97e356b02c3bbe67672d44adf6be7a2e2bc0b3e
stormeus <storm3us@gmail.com>
2013-03-31 04:34:20 -0400

    + Forgot to commit CTimer-specific files earlier

---
+++ CTimer.cpp
@@ -0,0 +1,47 @@
+#include "CCore.h"
+#include "CTimer.h"
+
+void  CTimer::Delete()          { this->committingSeppuku = true; }
+float CTimer::GetElapsedTicks() { return this->ticksElapsed; }
+
+bool  CTimer::Pulse( float elapsedTime )
+{
+	this->ticksElapsed += elapsedTime;
+
+	if( this->committingSeppuku )
+		return true;
+	else if( this->ticksElapsed >= this->intervalInTicks )
+	{
+		this->ticksElapsed = 0.0f;
+		this->pulseCount++;
+		this->pFunc.Execute();
+
+		if( this->maxNumberOfPulses > 0 && this->pulseCount >= this->maxNumberOfPulses )
+		{
+			// Assisted suicide
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void RegisterTimer()
+{
+	Class<CTimer> c(v);
+		
+	// Read-only properties
+	c
+		.Prop( _SC("Elapsed"), &CTimer::GetElapsedTicks );
+
+	// Functions
+	c
+		.Func( _SC("Delete"), &CTimer::Delete );
+
+	// Freely editable variables
+	c
+		.Var( _SC("Interval"), &CTimer::intervalInTicks )
+		.Var( _SC("Paused"), &CTimer::isPaused );
+
+	RootTable(v).Bind( _SC("CTimer"), c );
+}
---
+++ CTimer.h
@@ -0,0 +1,54 @@
+#include "main.h"
+#pragma once
+
+// Define structures for timer maps
+typedef struct
+{
+	void * pData;
+	unsigned char datatype; // This is some shitty programming yo
+} TimerParam;
+
+class CTimer
+{
+	public:
+		CTimer()
+		{
+			this->committingSeppuku = false;
+			this->ticksElapsed = 0.0f;
+			this->pulseCount = 0;
+			this->isPaused = false;
+		}
+		
+		bool Pulse( float elapsedTime );
+		
+		// Is this timer paused?
+		bool isPaused;
+
+		// The interval to run it at
+		float intervalInTicks;
+
+	public:
+		float GetElapsedTicks();
+		void Delete();
+
+	public:
+		// Ticks elapsed since last pulse
+		float ticksElapsed;
+
+		// Number of times to run, or 0 for an infinite number of runs.
+		unsigned int maxNumberOfPulses;
+
+		// How many times has this timer ran?
+		unsigned int pulseCount;
+
+		// The script function to use
+		Sqrat::Function pFunc;
+
+		// The list of parameters
+		TimerParam * params;
+
+	private:
+		bool committingSeppuku;
+};
+
+void RegisterTimer();
